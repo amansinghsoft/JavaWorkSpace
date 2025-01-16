7b44c80ef96f3706beacb097754c519f90a41490
amansinghsoft <aman.singh28912@gmail.com>
2025-01-16 22:48:10 +0530

    update

---
+++ Practice/src/practice/PatternExample03.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class PatternExample03 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 6; i++) {
+			for (int j = 0; j <= i/2; j++) {
+				System.out.print(" * ");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
